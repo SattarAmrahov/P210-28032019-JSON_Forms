9b595e227ab401152cba49f8eabd3ed8ffb85a57
Sattar Amrahov <sattar@code.local>
2019-03-28 17:53:53 +0400

    files added

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # P210-28032019-JSON_Forms
+
+Dersde baxilan linkler:
+
+- https://www.w3schools.com/js/js_json.asp 
+- https://www.w3schools.com/js/js_validation.asp
+- https://www.w3schools.com/js/js_validation_api.asp
+- https://www.w3schools.com/js/js_object_definition.asp
+- https://www.w3schools.com/js/js_object_properties.asp
+- https://www.w3schools.com/js/js_object_methods.asp
+- https://www.w3schools.com/js/js_object_accessors.asp
+- https://regexr.com/
---
+++ js/forms.js
@@ -0,0 +1,40 @@
+// "use strict";
+
+var x = 3.14;
+
+// function x(){}
+delete x;
+
+console.log(x);
+
+// var person = {
+//     firstname:"John",
+//     lastname:"Doe",
+//     age:50,
+//     eyecolor:"blue"
+//   };
+
+//   delete person.age;
+//  console.log( person.firstname + " is " + person.age + " years old.");
+
+//  person.age = 99;
+
+//  console.log(person);
+
+// let myObj = {
+//     name: "Rauf",
+//     surname: "Babazade",
+//     get getSurname() {
+//         return this.surname;
+//       },
+// };
+
+// Object.defineProperty(myObj, "fullname", {
+//     get: function(){return this.name + " - " + this.surname}
+// });
+// Object.defineProperty(myObj, "modifyName", {
+//     set: function(newName){ this.name = newName}
+// });
+
+// myObj.modifyName = "Rauf2";
+// console.log(myObj);
